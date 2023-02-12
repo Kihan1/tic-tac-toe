a95febfd5d9ed9f2ba7fc6c83aef7a1ec98a4733
Allen Kileo <96255927+Kihan1@users.noreply.github.com>
2023-02-12 08:06:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+/* eventlistener on the window object to listen to the DOMContentLoaded, this is needed because 
+we included our js file on the head of our HTML doc this needs to be processed b4 any HTML*/
 window.addEventListener('DOMContentLoaded',()=>{
     const tiles=Array.from(document.querySelectorAll('.tile'));
     const playerDisplay=document.querySelector('.display-player');
     const resetButton=document.querySelector('#reset');
     const announcer=document.querySelector('.announcer');
 
+    // add the variables we will need
     let board = ['','','','','','','','',''];
     let currentPlayer ='X';
     let isGameActive=true;
 
+    // these are our constant values
     const PLAYERX_WON ='PLAYERX_WON';
     const PLAYERO_WON ='PLAYERO_WON';
     const TIE ='TIE';
@@ -31,7 +35,7 @@ window.addEventListener('DOMContentLoaded',()=>{
         [2,4,6]
     ];
 
-
+    //checking ifwe have a winner or not by looping through the wincondition
     function handleResultValidation() {
         let roundWon = false;
         for (let i=0; i<=7; i++){
@@ -39,26 +43,28 @@ window.addEventListener('DOMContentLoaded',()=>{
             const a = board[winCondition[0]];
             const b = board[winCondition[1]];
             const c = board[winCondition[2]];
+            //if any of the three elements is an empty string we will skipp the iteration
             if (a === '' || b === '' || c ==='') {
                 continue;
             }
+            //if they are equal then we set roundwon to true
             if (a ===b && b ===c) {
                 roundWon = true;
                 break;
             }
         }
-
+       //if we have a winner then the anounce function will say who won
         if (roundWon) {
             announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
             isGameActive =false;
             return;
         }
-
+    //if we dont have any winners and there are no empty spaces left then its a tie
         if (!board.includes(''))
         announce(TIE);
     }
 
-
+//announce our winner
     const announce = (type) =>{
         switch(type){
             case PLAYERO_WON:
@@ -70,9 +76,10 @@ window.addEventListener('DOMContentLoaded',()=>{
             case TIE:
                 announcer.innerText = 'Tie';
         }
+        // remove the hide class to show the user who won
         announcer.classList.remove('hide');
     };
-
+//this is used to check if the tile has a value already
     const isValidAction = (tile) => {
         if(tile.innerText === 'X' || tile.innnerText ==='O'){
             return false;
@@ -80,11 +87,13 @@ window.addEventListener('DOMContentLoaded',()=>{
 
         return true;
     };
-
+    /*it sends the value of index array to board array and the current position 
+    to ebe equal to the value of the current player variable*/
     const updateBoard = (index) =>{
         board[index] = currentPlayer;
     }
 
+    //change current player to be o if it was x and vice versa
     const changePlayer = () =>{
         playerDisplay.classList.remove('player${currentPlayer}');
         currentPlayer = currentPlayer === 'X' ? 'O' :'X';
@@ -92,6 +101,7 @@ window.addEventListener('DOMContentLoaded',()=>{
         playerDisplay.classList.add('player${currentPlayer}');
     }
 
+    // implement user action function when user 
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive) {
             tile.innerText = currentPlayer;
@@ -101,7 +111,7 @@ window.addEventListener('DOMContentLoaded',()=>{
             changePlayer();
         }
     }
-
+    // this will help reset the game state
     const resetBoard = () => {
         board = ['','','','','','','','',''];
         isGameActive = true;
@@ -110,7 +120,7 @@ window.addEventListener('DOMContentLoaded',()=>{
         if (currentPlayer === 'O'){
             changePlayer();
         }
-
+//here we will set all inner text to be empty and remove all player related values
         tiles.forEach (tile =>{
             tile.innerText ='';
             tile.classList.remove('playerX');
@@ -118,11 +128,11 @@ window.addEventListener('DOMContentLoaded',()=>{
         });
     }
 
-
+    // with refference to the specific tile ,index will be used to update our in memory saved
     tiles.forEach( (tile,index)=>{
         tile.addEventListener('click', ()=>userAction(tile,index));
     });
 
-
+// this line allows us to reset the score and restart the game afresh
     resetButton.addEventListener('click', resetBoard);
 });
